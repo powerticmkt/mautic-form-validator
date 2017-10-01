9d8ae51b674616cd8d2c2c8e7e9a59041e13a74d
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2017-10-01 20:21:04 -0300

    Plugin WP

--- plugin.php
+++ plugin.php
@@ -8,6 +8,10 @@ Version: 1.0.0-beta
 Author URI: https://powertic.com
 */
 
+wp_register_script( 'powertic-mautic-form-validator-js', 'https://cdn.rawgit.com/powerticmkt/mautic-form-validator/8b24d40e/dist/powertic-mautic-form-validator.js' , '', '', false );
+wp_enqueue_script( 'powertic-mautic-form-validator-js' );
+wp_enqueue_style("powertic-mautic-form-validator-css", "https://cdn.rawgit.com/powerticmkt/mautic-form-validator/8b24d40e/dist/powertic-mautic-form-validator.css");
+
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/powerticmkt/mautic-form-validator/',
