4fb887e99939ab7d6340c1f9976be4f02220c69f
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2017-10-01 18:32:21 -0300

    Adicionando o BOM para evitar problemas com acentuação

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
-var strip = require('gulp-strip-comments');
-var uglify = require('gulp-uglify');
+var strip = require('gulp-strip-comments')
+var uglify = require('gulp-uglify')
+var bom = require('gulp-bom')
 
 gulp.task('js', function () {
   return gulp.src([
-      'js/credits.js',
-      'node_modules/jquery/dist/jquery.js',
-      'node_modules/jquery-confirm/dist/jquery-confirm.min.js',
-      'node_modules/mailcheck/src/mailcheck.min.js',
-      'js/app.js'])
-      .pipe(strip())
-      .pipe(uglify())
-     .pipe(concat({ path: 'powertic-mautic-form-validator.js', stat: { mode: 0666 }}))
+    'js/credits.js',
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery-confirm/dist/jquery-confirm.min.js',
+    'node_modules/mailcheck/src/mailcheck.min.js',
+    'js/app.js'])
+    .pipe(concat({ path: 'powertic-mautic-form-validator.js'}))
+    .pipe(bom())
     .pipe(gulp.dest('./dist/'))
 })
--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
-jQuery('input[type=email]').on('blur', function () {
-  jQuery(this).mailcheck({
-    suggested: function (element, suggestion) {
-      jQuery.confirm({
-        theme: 'supervan',
-        title: 'O e-mail ' + jQuery('input[type=email]').val() + ' é válido?',
-        content: 'Parece que você digitou um e-mail que não existe. Posso alterar para ' + suggestion.full + '?',
-        buttons: {
-          confirm: {
-            text: 'Sim',
-            action: function () {
-              jQuery('input[type=email]').val(suggestion.full).focus()
+jQuery(document).ready(function () {
+  alert('teste')
+  jQuery('input[type=email]').on('blur', function () {
+    jQuery(this).mailcheck({
+      suggested: function (element, suggestion) {
+        jQuery.confirm({
+          theme: 'supervan',
+          title: 'O e-mail ' + jQuery('input[type=email]').val() + ' é válido?',
+          content: 'Parece que você digitou um e-mail que não existe. Posso alterar para ' + suggestion.full + '?',
+          buttons: {
+            confirm: {
+              text: 'Sim',
+              action: function () {
+                jQuery('input[type=email]').val(suggestion.full).focus()
+              }
+            },
+            cancel: {
+              text: 'Não',
+              action: function () {}
             }
-          },
-          cancel: {
-            text: 'Não',
-            action: function () {}
           }
-        }
-      })
-    },
-    empty: function (element) {
-      // callback code
-    }
+        })
+      },
+      empty: function (element) {
+        // callback code
+      }
+    })
   })
 })
